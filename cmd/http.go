b1870b53c56db0949f43e744f92e6bdae91b2d0a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-21 17:20:23 +0330

    make modification to cmd/http.go

--- cmd/http.go
+++ cmd/http.go
@@ -11,7 +11,7 @@ var httpCmd = &cobra.Command{
 	Use:   "http",
 	Short: "Launching ShopSphere App",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Server is running on port%s", config.AppConfig.ServerConfig.Port)
+		fmt.Printf("Server is running on %s", config.AppConfig.ServerConfig.Port)
 		if err := gateway.Server(); err != nil {
 			panic(err)
 		}
