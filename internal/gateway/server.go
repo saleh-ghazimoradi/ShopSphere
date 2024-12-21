be339db2242482a03a438a08d67df9cd269148c4
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-21 22:53:02 +0330

    add db connection to the server

--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -3,12 +3,22 @@ package gateway
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/saleh-ghazimoradi/ShopSphere/config"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/gateway/rest/routes"
+	"github.com/saleh-ghazimoradi/ShopSphere/utils"
 )
 
 func Server() error {
-
 	app := fiber.New()
-	if err := app.Listen(config.AppConfig.ServerConfig.Port); err != nil {
+
+	db, err := utils.DBConnection(utils.DBMigrator)
+	if err != nil {
+		return err
+	}
+
+	routes.HealthCheck(app)
+	routes.UserRoutes(app, db)
+
+	if err = app.Listen(config.AppConfig.ServerConfig.Port); err != nil {
 		return err
 	}
 	return nil
