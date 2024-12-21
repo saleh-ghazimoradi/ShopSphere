6586ec0802031a17ca907a48bce3c625354528a5
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-21 15:57:01 +0330

    add cmd http.go file to the project for launching the app

---
+++ cmd/http.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/saleh-ghazimoradi/ShopSphere/config"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/gateway"
+	"github.com/spf13/cobra"
+)
+
+var httpCmd = &cobra.Command{
+	Use:   "http",
+	Short: "Launching ShopSphere App",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Server is running on port%s", config.AppConfig.ServerConfig.Port)
+		if err := gateway.Server(); err != nil {
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(httpCmd)
+}
