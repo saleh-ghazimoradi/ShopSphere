36b9b4f7e260527e90c65aab0a2726ce7551ed26
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-22 18:07:28 +0330

    implement some user functionalities and add the authorization middleware to the user routes

--- internal/gateway/rest/handlers/user.go
+++ internal/gateway/rest/handlers/user.go
@@ -3,12 +3,14 @@ package handlers
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/dto"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/helper"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/service"
 	"net/http"
 )
 
 type UserHandler struct {
 	userService service.User
+	authService helper.Auth
 }
 
 func (u *UserHandler) Register(ctx *fiber.Ctx) error {
@@ -29,52 +31,88 @@ func (u *UserHandler) Register(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": token,
+		"message": "register",
+		"token":   token,
 	})
 }
+
 func (u *UserHandler) Login(ctx *fiber.Ctx) error {
+	loginInput := dto.UserLogin{}
+
+	if err := ctx.BodyParser(&loginInput); err != nil {
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "please provide valid input",
+		})
+	}
+
+	token, err := u.userService.Login(ctx.Context(), loginInput.Email, loginInput.Password)
+	if err != nil {
+		return ctx.Status(http.StatusUnauthorized).JSON(fiber.Map{
+			"message": "please provide correct user id password",
+		})
+	}
+
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
+		"token":   token,
 	})
+
 }
+
 func (u *UserHandler) GetVerificationCode(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
 	})
 }
+
 func (u *UserHandler) Verify(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
 	})
 }
+
 func (u *UserHandler) CreateProfile(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
 	})
 }
+
 func (u *UserHandler) GetProfile(ctx *fiber.Ctx) error {
+	user, err := u.authService.GetCurrentUser(ctx)
+	if err != nil {
+		return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "error get current user",
+		})
+	}
+
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "login",
+		"message": "get profile",
+		"user":    user,
 	})
 }
+
 func (u *UserHandler) AddToCart(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
 	})
 }
+
 func (u *UserHandler) GetCart(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
 	})
 }
+
 func (u *UserHandler) CreateOrder(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{})
 }
+
 func (u *UserHandler) GetOrders(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "login",
 	})
 }
+
 func (u *UserHandler) GetOrder(ctx *fiber.Ctx) error {
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{})
 }
@@ -84,8 +122,9 @@ func (u *UserHandler) BecomeSeller(ctx *fiber.Ctx) error {
 	})
 }
 
-func NewUserHandler(userService service.User) *UserHandler {
+func NewUserHandler(userService service.User, authService helper.Auth) *UserHandler {
 	return &UserHandler{
 		userService: userService,
+		authService: authService,
 	}
 }
--- internal/gateway/rest/routes/user.go
+++ internal/gateway/rest/routes/user.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/saleh-ghazimoradi/ShopSphere/config"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/gateway/rest/handlers"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/helper"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/repository"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/service"
 	"gorm.io/gorm"
@@ -10,18 +12,24 @@ import (
 
 func UserRoutes(app *fiber.App, db *gorm.DB) {
 	userRepository := repository.NewUserRepository(db)
-	userService := service.NewUserService(userRepository)
-	user := handlers.NewUserHandler(userService)
+	authService := helper.NewAuth(config.AppConfig.AppSecret.Secret)
+	userService := service.NewUserService(userRepository, authService)
+	user := handlers.NewUserHandler(userService, authService)
 
-	app.Post("/register", user.Register)
-	app.Post("/login", user.Login)
-	app.Get("/verify", user.GetVerificationCode)
-	app.Post("/verify", user.Verify)
-	app.Post("/profile", user.CreateProfile)
-	app.Get("/profile", user.GetProfile)
-	app.Post("/cart", user.AddToCart)
-	app.Get("/cart", user.GetCart)
-	app.Get("/order", user.GetOrders)
-	app.Get("/order/:id", user.GetOrder)
-	app.Post("/become-seller", user.BecomeSeller)
+	// Public routes
+	pubRoutes := app.Group("/users")
+	pubRoutes.Post("/register", user.Register)
+	pubRoutes.Post("/login", user.Login)
+
+	// Private routes
+	pvtRoutes := pubRoutes.Group("/", authService.Authorize)
+	pvtRoutes.Get("/verify", user.GetVerificationCode)
+	pvtRoutes.Post("/verify", user.Verify)
+	pvtRoutes.Post("/profile", user.CreateProfile)
+	pvtRoutes.Get("/profile", user.GetProfile)
+	pvtRoutes.Post("/cart", user.AddToCart)
+	pvtRoutes.Get("/cart", user.GetCart)
+	pvtRoutes.Get("/order", user.GetOrders)
+	pvtRoutes.Get("/order/:id", user.GetOrder)
+	pvtRoutes.Post("/become-seller", user.BecomeSeller)
 }
