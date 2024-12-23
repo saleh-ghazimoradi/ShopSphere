30b7fe53413f38b3dce0d1ea7686b9f2e09ac122
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-23 23:53:40 +0330

    add seller input to the dto directory

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -13,3 +13,12 @@ type UserSignUp struct {
 type VerificationCodeInput struct {
 	Code int `json:"code"`
 }
+
+type SellerInput struct {
+	Firstname         string `json:"first_name"`
+	Lastname          string `json:"last_name"`
+	PhoneNumber       string `json:"phone_number"`
+	BankAccountNumber string `json:"bank_account_number"`
+	SwiftCode         string `json:"swift_code"`
+	PaymentType       string `json:"payment_type"`
+}
