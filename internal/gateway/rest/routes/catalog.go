0f9861121a3a241bceaf7b96b412e25ebc435e71
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-24 16:01:35 +0330

    implement routing for the catalog

---
+++ internal/gateway/rest/routes/catalog.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/saleh-ghazimoradi/ShopSphere/config"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/gateway/rest/handlers"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/helper"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/repository"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/service"
+	"gorm.io/gorm"
+)
+
+func CatalogRoutes(app *fiber.App, db *gorm.DB) {
+	catalogRepository := repository.NewCatalogRepository(db)
+	catalogService := service.NewCatalogService(catalogRepository)
+	authService := helper.NewAuth(config.AppConfig.AppSecret.Secret)
+	catalog := handlers.NewCatalogHandler(catalogService, authService)
+
+	// Listing Products and Categories
+	app.Get("/products")
+	app.Get("/products/:id")
+	app.Get("/categories")
+	app.Get("/categories/:id")
+
+	sellerRoutes := app.Group("/sellers", authService.AuthorizeSeller)
+	// Categories
+	sellerRoutes.Post("/categories", catalog.CreateCategories)
+	sellerRoutes.Patch("/categories/:id", catalog.EditCategory)
+	sellerRoutes.Delete("categories/:id", catalog.DeleteCategory)
+
+	// Products
+	sellerRoutes.Post("/products", catalog.CreateProducts)
+	sellerRoutes.Get("/products", catalog.GetProducts)
+	sellerRoutes.Get("/products/:id", catalog.GetProduct)
+	sellerRoutes.Put("/products/:id", catalog.EditProduct)
+	sellerRoutes.Patch("/products/:id", catalog.UpdateStock)
+	sellerRoutes.Delete("/products/:id", catalog.DeleteProduct)
+}
