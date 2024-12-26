4814b9d3c71c332e88943b6968f3397324cdbd25
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-26 16:00:20 +0330

    implement product functionalities in the repository layer

--- internal/repository/catalog.go
+++ internal/repository/catalog.go
@@ -13,6 +13,13 @@ type Catalog interface {
 	FindCategoryById(ctx context.Context, id int) (*serviceModels.Category, error)
 	EditCategory(ctx context.Context, category *serviceModels.Category) (*serviceModels.Category, error)
 	DeleteCategory(ctx context.Context, id int) error
+
+	CreateProduct(ctx context.Context, product *serviceModels.Product) error
+	FindProducts(ctx context.Context) ([]*serviceModels.Product, error)
+	FindProductById(ctx context.Context, id int) (*serviceModels.Product, error)
+	FindSellerProducts(ctx context.Context, id int) ([]*serviceModels.Product, error)
+	EditProduct(ctx context.Context, product *serviceModels.Product) (*serviceModels.Product, error)
+	DeleteProduct(ctx context.Context, id int) error
 }
 
 type catalogRepository struct {
@@ -57,6 +64,57 @@ func (c *catalogRepository) EditCategory(ctx context.Context, category *serviceM
 func (c *catalogRepository) DeleteCategory(ctx context.Context, id int) error {
 	if err := c.db.WithContext(ctx).Delete(&serviceModels.Category{}, id).Error; err != nil {
 		return errors.New("delete category failed")
+	}
+	return nil
+}
+
+func (c *catalogRepository) CreateProduct(ctx context.Context, product *serviceModels.Product) error {
+	err := c.db.WithContext(ctx).Create(product).Error
+	if err != nil {
+		return errors.New("create product failed")
+	}
+	return nil
+}
+
+func (c *catalogRepository) FindProducts(ctx context.Context) ([]*serviceModels.Product, error) {
+	var products []*serviceModels.Product
+	err := c.db.WithContext(ctx).Find(&products).Error
+	if err != nil {
+		return nil, errors.New("find products failed")
+	}
+	return products, nil
+}
+
+func (c *catalogRepository) FindProductById(ctx context.Context, id int) (*serviceModels.Product, error) {
+	var product *serviceModels.Product
+	err := c.db.WithContext(ctx).First(&product, id).Error
+	if err != nil {
+		return nil, errors.New("product does not exist")
+	}
+	return product, nil
+}
+
+func (c *catalogRepository) FindSellerProducts(ctx context.Context, id int) ([]*serviceModels.Product, error) {
+	var products []*serviceModels.Product
+	err := c.db.WithContext(ctx).Where("user_id = ?", id).Find(&products).Error
+	if err != nil {
+		return nil, errors.New("find products failed")
+	}
+	return products, nil
+}
+
+func (c *catalogRepository) EditProduct(ctx context.Context, product *serviceModels.Product) (*serviceModels.Product, error) {
+	err := c.db.WithContext(ctx).Save(&product).Error
+	if err != nil {
+		return nil, errors.New("edit product failed")
+	}
+	return product, nil
+}
+
+func (c *catalogRepository) DeleteProduct(ctx context.Context, id int) error {
+	err := c.db.WithContext(ctx).Delete(&serviceModels.Product{}, id).Error
+	if err != nil {
+		return errors.New("delete product failed")
 	}
 	return nil
 }
