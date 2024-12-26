8d6b09436d2e48bdd075523da0c2c1037d1dcde0
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-26 15:59:24 +0330

    implement the product functionalities in the application layer

--- internal/gateway/rest/handlers/catalog.go
+++ internal/gateway/rest/handlers/catalog.go
@@ -6,6 +6,7 @@ import (
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/dto"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/helper"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/service"
+	"github.com/saleh-ghazimoradi/ShopSphere/internal/service/serviceModels"
 	"net/http"
 	"strconv"
 )
@@ -70,7 +71,6 @@ func (c *CatalogHandler) EditCategory(ctx *fiber.Ctx) error {
 	}
 
 	return successMessage(ctx, "edit category", updatedCate)
-
 }
 
 func (c *CatalogHandler) DeleteCategory(ctx *fiber.Ctx) error {
@@ -82,27 +82,87 @@ func (c *CatalogHandler) DeleteCategory(ctx *fiber.Ctx) error {
 }
 
 func (c *CatalogHandler) CreateProducts(ctx *fiber.Ctx) error {
-	return successMessage(ctx, "Create product endpoint", nil)
+	req := dto.Product{}
+	if err := ctx.BodyParser(&req); err != nil {
+		return badRequestError(ctx, "invalid request body")
+	}
+
+	user, _ := c.authService.GetCurrentUser(ctx)
+
+	if err := c.catalogService.CreateProduct(context.Background(), &req, user); err != nil {
+		return internalError(ctx, err)
+	}
+
+	return successMessage(ctx, "Create Product", nil)
 }
 
 func (c *CatalogHandler) GetProduct(ctx *fiber.Ctx) error {
-	return successMessage(ctx, "Get product endpoint", nil)
+	id, _ := strconv.Atoi(ctx.Params("id"))
+	product, err := c.catalogService.GetProduct(context.Background(), id)
+	if err != nil {
+		return errorMessage(ctx, http.StatusNotFound, err)
+	}
+	return successMessage(ctx, "Product", product)
 }
 
 func (c *CatalogHandler) GetProducts(ctx *fiber.Ctx) error {
-	return successMessage(ctx, "Get product endpoint", nil)
+	products, err := c.catalogService.GetProducts(context.Background())
+	if err != nil {
+		return errorMessage(ctx, http.StatusNotFound, err)
+	}
+	return successMessage(ctx, "Products", products)
 }
 
 func (c *CatalogHandler) EditProduct(ctx *fiber.Ctx) error {
-	return successMessage(ctx, "Edit product endpoint", nil)
+	id, _ := strconv.Atoi(ctx.Params("id"))
+	req := dto.UpdateProduct{}
+	if err := ctx.BodyParser(&req); err != nil {
+		return badRequestError(ctx, "invalid request body")
+	}
+
+	user, _ := c.authService.GetCurrentUser(ctx)
+
+	product, err := c.catalogService.EditProduct(context.Background(), id, &req, user)
+	if err != nil {
+		return internalError(ctx, err)
+	}
+
+	return successMessage(ctx, "EditProduct", product)
 }
 
 func (c *CatalogHandler) UpdateStock(ctx *fiber.Ctx) error {
-	return successMessage(ctx, "Update stock endpoint", nil)
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	req := dto.UpdateStock{}
+	if err := ctx.BodyParser(&req); err != nil {
+		return badRequestError(ctx, "invalid request body")
+	}
+
+	user, _ := c.authService.GetCurrentUser(ctx)
+
+	product := serviceModels.Product{
+		ID:     uint(id),
+		Stock:  uint(*req.Stock),
+		UserId: user.ID,
+	}
+
+	updatedProduct, err := c.catalogService.UpdateProductStock(context.Background(), &product)
+	if err != nil {
+		return internalError(ctx, err)
+	}
+
+	return successMessage(ctx, "Update Stock", updatedProduct)
 }
 
 func (c *CatalogHandler) DeleteProduct(ctx *fiber.Ctx) error {
-	return successMessage(ctx, "Delete product endpoint", nil)
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	user, _ := c.authService.GetCurrentUser(ctx)
+	if err := c.catalogService.DeleteProduct(context.Background(), id, user); err != nil {
+		return errorMessage(ctx, http.StatusNotFound, err)
+	}
+
+	return successMessage(ctx, "Delete product", nil)
 }
 
 func NewCatalogHandler(catalogService service.Catalog, authService helper.Auth) *CatalogHandler {
