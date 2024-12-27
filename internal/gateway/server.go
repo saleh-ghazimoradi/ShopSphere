939bfc9805b19d7d15ba878a9e8aab3fdf0e0cce
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-27 14:02:47 +0330

    add CORS to the server

--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -2,6 +2,7 @@ package gateway
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/saleh-ghazimoradi/ShopSphere/config"
 	"github.com/saleh-ghazimoradi/ShopSphere/internal/gateway/rest/routes"
 	"github.com/saleh-ghazimoradi/ShopSphere/utils"
@@ -14,6 +15,14 @@ func Server() error {
 	if err != nil {
 		return err
 	}
+
+	c := cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3030",
+		AllowHeaders: "Content-Type, Accept, Authorization",
+		AllowMethods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+	})
+
+	app.Use(c)
 
 	routes.HealthCheck(app)
 	routes.UserRoutes(app, db)
